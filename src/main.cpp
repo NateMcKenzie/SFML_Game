d8da91402d3484d2c51d855bb5b2e90a9f37104d
Nate McKenzie <nate6264@gmail.com>
2025-01-02 17:47:26 -0700

    New base, fix clangd 'errors'

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@ int main() {
     auto window = sf::RenderWindow(sf::VideoMode({ 1920u, 1080u }), "CMake SFML Project");
     window.setFramerateLimit(144);
 
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Red);
+
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
@@ -12,6 +15,7 @@ int main() {
         }
 
         window.clear();
+        window.draw(shape);
         window.display();
     }
 }
